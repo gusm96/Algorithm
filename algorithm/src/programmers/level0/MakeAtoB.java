f4f505897092b058d13140156fd675771270c622
Seongmo Gu <77833389+gusm96@users.noreply.github.com>
2023-03-04 15:07:51 +0900

    Update 230304

---
+++ algorithm/src/programmers/level0/MakeAtoB.java
@@ -0,0 +1,23 @@
+package programmers.level0;
+
+import java.util.Arrays;
+
+public class MakeAtoB {
+	public int solution(String before, String after) {
+		int answer = 1;
+		String [] a = after.split("");
+		String [] b = before.split("");
+		Arrays.sort(a);
+		Arrays.sort(b);
+		for(int i = 0; i < a.length; i++) {
+			if(!a[i].equals(b[i])) return 0;
+		}
+		return answer;
+	}
+	public static void main (String[] args) {
+		String before ="olleh";
+		String after = "hello";
+		MakeAtoB mab = new MakeAtoB();
+		System.out.println(mab.solution(before, after));
+	}
+}
