e4281661dd479596ff2f7ae7333958515dfbe896
Seongmo Gu <77833389+gusm96@users.noreply.github.com>
2023-03-24 23:52:10 +0900

    Update230324

--- algorithm/src/string/BaekJoon11718.java
+++ algorithm/src/string/BaekJoon11718.java
@@ -17,7 +17,7 @@ public class BaekJoon11718 {
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = "";
-		while((str = br.readLine())!=null) {
+		while((str = br.readLine()).isEmpty()) {
 			System.out.println(str);
 		}
 	}
